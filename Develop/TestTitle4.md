1d6b14fa7a2a47e5c721463b969de6330a139023
Dave Bean <contact@davebean.net>
2020-07-25 21:17:14 -0600

    writing to file

---
+++ Develop/TestTitle4.md
@@ -0,0 +1,20 @@
+## Description
+
+Test Description 4
+
+		## Table of Contents
+
+
+		Installation
+
+		Usage Information
+
+		Contribution Guidelines
+
+		Test Instructions
+
+		## Installation
+
+Installation Instructions 4
+# TestTitle4
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 var inquirer = require("inquirer");
+fs = require('fs');
 
 // array of questions for user
 const questions = [
@@ -12,16 +13,13 @@ const questions = [
 		message: "What is your project's Description?",
 		name: "description"
 	  },
+	
 	{
-		type: "editor",
-		message: "What is your Table of Contents?",
-		name: "table_of_contents"
-	},
-	  {
 		  type: "editor",
 		  message: "What are your Installation Instructions?",
-		  name: "inst_instruct"
-	  },
+		  name: "installation"
+	}
+	/*
 	{
 		type: "editor",
 		message: "What is your Usage Information?",
@@ -37,10 +35,16 @@ const questions = [
 		message: "What are your Test Instructions?",
 		name: "test_instructions"
 	}
+	*/
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+	fs.appendFile(`${fileName}.md`, data, function (err) {
+	if (err) return console.log(err);
+		console.log('Error writing to file.');
+	});
+	process.exit();
 }
 
 // function to initialize program
@@ -49,9 +53,31 @@ function init(questions) {
 	.prompt(questions)
 	.then(function(response) {
 
-	console.log(response.title);
-	console.log(response.description);
-	console.log(response.inst_instruct);
+	const sanitizedTitle = response.title;
+
+	const data = `# ${response.title}\n\n## Description\n\n${response.description}\n## Table of Contents\n\nInstallation\nUsage Information\nContribution Guidelines\nTest Instructions\n## Installation\n\n${response.installation}\n`
+
+	writeToFile(sanitizedTitle, data);
+	
+	/*
+	const title = `# ${response.title}\n\n`;
+	writeToFile(sanitizedTitle, title);	
+
+	const description = `## Description\n\n${response.description}\n`;
+	writeToFile(sanitizedTitle, description);
+
+	const table_of_contents = `
+		## Table of Contents\n\n
+		Installation\n
+		Usage Information\n
+		Contribution Guidelines\n
+		Test Instructions\n
+		`;
+	writeToFile(sanitizedTitle, table_of_contents);
+
+	const installation = `## Installation\n\n${response.installation}\n`;
+	writeToFile(sanitizedTitle, installation);
+	*/
 
   });
 }
